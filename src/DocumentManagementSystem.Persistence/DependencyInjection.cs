21431ee613fb2cc7e046668d34c54accc1976005
Mark-Stessl <stesslmark4@gmail.com>
2026-09-22 13:34:18 +0200

    PostgreSQL und EF Core integrieren

--- src/DocumentManagementSystem.Persistence/DependencyInjection.cs
+++ src/DocumentManagementSystem.Persistence/DependencyInjection.cs
@@ -1,13 +1,18 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace DocumentManagementSystem.Persistence;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        string connectionString)
     {
-        services.AddSingleton<IDocumentRepository, InMemoryDocumentRepository>();
-        services.AddSingleton<ICollectionRepository, InMemoryCollectionRepository>();
+        services.AddDbContext<DocumentManagementDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        services.AddScoped<IDocumentRepository, DocumentRepository>();
+        services.AddScoped<ICollectionRepository, CollectionRepository>();
         return services;
     }
 }
